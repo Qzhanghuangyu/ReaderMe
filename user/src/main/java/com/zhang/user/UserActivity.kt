8c2a59d36dd0f93deae4643613caee01fe624636
zhy <zhanghuanyu@iyinguo.com>
2025-06-21 18:12:11 +0800

    First Commit

---
+++ user/src/main/java/com/zhang/user/UserActivity.kt
@@ -0,0 +1,11 @@
+package com.zhang.user
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserActivity :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_user)
+    }
+}
