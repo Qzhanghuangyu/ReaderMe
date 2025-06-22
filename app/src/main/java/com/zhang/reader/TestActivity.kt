f2d37749a3f06cba198e2c6b98ef3bec1283f1d9
zhy <zhanghuanyu@iyinguo.com>
2025-06-22 15:11:12 +0800

    arouter接入

--- app/src/main/java/com/zhang/reader/TestActivity.kt
+++ app/src/main/java/com/zhang/reader/TestActivity.kt
@@ -1,13 +1,13 @@
-package com.zhang.user
+package com.zhang.reader
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.facade.annotation.Route
 
-@Route(path = "/user/activity")
-class UserActivity :AppCompatActivity() {
+@Route(path = "/app/activity", name = "ceshi")
+class TestActivity :Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_user)
+    setContentView(R.layout.activity_test)
     }
 }
